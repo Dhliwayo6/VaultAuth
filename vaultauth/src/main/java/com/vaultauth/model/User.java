c2140a41c0c438630b824b4c8946433e4095cddc
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-11-29 23:49:50 +0200

    Add basic user entity class

---
+++ vaultauth/src/main/java/com/vaultauth/model/User.java
@@ -0,0 +1,70 @@
+package com.vaultauth.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "app_users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "email", unique = true)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "verification_code")
+    private String verificationCode;
+    @Column(name = "verification_code_expiry")
+    private LocalDateTime verificationExpiry;
+    private boolean enabled;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
+
+
+
+
