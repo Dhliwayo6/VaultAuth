b0d31753b6e7be2b0ac96b66bc07be42839a6691
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-12-01 23:44:44 +0200

    Add register user dto

---
+++ vaultauth/src/main/java/com/vaultauth/dtos/RegisterUserDTO.java
@@ -0,0 +1,23 @@
+package com.vaultauth.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RegisterUserDTO {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "Password must have an uppercase letter, a lowercase letter, a digit and at least one special character")
+    private String password;
+    private String username;
+}
