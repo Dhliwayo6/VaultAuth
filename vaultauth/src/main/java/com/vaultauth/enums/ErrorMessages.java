fbec5a732de7b443eb13b6e83ea90ecda6013868
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-12-09 20:21:45 +0200

    Add error messages for custom exceptions

---
+++ vaultauth/src/main/java/com/vaultauth/enums/ErrorMessages.java
@@ -0,0 +1,22 @@
+package com.vaultauth.enums;
+
+public enum ErrorMessages {
+    USER_NOT_FOUND("User not found!"),
+    ACCOUNT_NOT_VERIFIED("Account not verified, verify account before logging in"),
+    EMAIL_ALREADY_EXISTS("The email you entered already exists"),
+    INVALID_TOKEN("Invalid token"),
+    ACCOUNT_ALREADY_VERIFIED("Account has already been verified"),
+    VERIFICATION_TOKEN_EXPIRED("The verification token you entered has expired"),
+    VALIDATION_ERROR("Validation error"),
+    INVALID_CREDENTIALS("Invalid username or password");
+
+    private String message;
+
+    ErrorMessages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
