4ef5d1b35ff63121b309586c9cb151369bec6ce3
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-12-09 20:40:35 +0200

    Update global exceptiom handler, handle expired verification token custom exception

--- vaultauth/src/main/java/com/vaultauth/exceptions/GlobalExceptionHandler.java
+++ vaultauth/src/main/java/com/vaultauth/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,22 @@
 package com.vaultauth.exceptions;
 
+import com.vaultauth.dtos.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(VerificationTokenExpiredException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ResponseBody
+    public ErrorResponse handleVerificationTokenExpiredException(VerificationTokenExpiredException exception) {
+        return new ErrorResponse(exception.getMessage());
+    }
+    
+
+
 }
